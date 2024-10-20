a03150786be1878cfb70721e221eaf88bde50813
wkfedor <wkfedor@gmail.com>
2024-10-20 18:42:04 +0700

    contents длинные индексы

--- db/migrate/20241017121213_create_contents.rb
+++ db/migrate/20241017121213_create_contents.rb
@@ -5,13 +5,17 @@ class CreateContents < ActiveRecord::Migration[5.2]
       t.references :setting, foreign_key: true, comment: "Ссылка на проект (Setting)"
 
       # Полиморфная связь с оригинальным объектом
-      t.references :original_source, polymorphic: true, index: true, comment: "Полиморфная связь с оригинальным источником информации (например, сообщение из Telegram, группы ВКонтакте, сайта)"
+      t.references :original_source, polymorphic: true, index: false, comment: "Полиморфная связь с оригинальным источником информации (например, сообщение из Telegram, группы ВКонтакте, сайта)"
 
       # Полиморфная связь с переработанным объектом
-      t.references :processed_content, polymorphic: true, index: true, comment: "Полиморфная связь с переработанным содержимым (например, переработанное сообщение)"
+      t.references :processed_content, polymorphic: true, index: false, comment: "Полиморфная связь с переработанным содержимым (например, переработанное сообщение)"
 
       t.timestamps
     end
+
+    # Явно создаем индексы с короткими именами
+    add_index :contents, [:original_source_type, :original_source_id], name: "idx_original_source"
+    add_index :contents, [:processed_content_type, :processed_content_id], name: "idx_processed_content"
   end
 
   def down
---
+++ db/schema.rb
@@ -0,0 +1,120 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_121213) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "censors", comment: "Таблица для хранения правил цензуры", force: :cascade do |t|
+    t.string "name", null: false, comment: "Название правила"
+    t.text "description", comment: "Описание правила"
+    t.text "check_data", comment: "Массив данных для проверки"
+    t.string "check_rule", null: false, comment: "Правило проверки (точное совпадение, частичное совпадение)"
+    t.string "action", null: false, comment: "Действие проверки (отклонить, предупредить)"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "contents", force: :cascade do |t|
+    t.string "status", comment: "Статус нового сообщения"
+    t.bigint "setting_id", comment: "Ссылка на проект (Setting)"
+    t.string "original_source_type"
+    t.bigint "original_source_id", comment: "Полиморфная связь с оригинальным источником информации (например, сообщение из Telegram, группы ВКонтакте, сайта)"
+    t.string "processed_content_type"
+    t.bigint "processed_content_id", comment: "Полиморфная связь с переработанным содержимым (например, переработанное сообщение)"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["original_source_type", "original_source_id"], name: "idx_original_source"
+    t.index ["processed_content_type", "processed_content_id"], name: "idx_processed_content"
+    t.index ["setting_id"], name: "index_contents_on_setting_id"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string "project_name", null: false, comment: "Название проекта"
+    t.text "project_description", comment: "Описание проекта"
+    t.jsonb "execution_sequence", default: [], null: false, comment: "Последовательность выполнения классов и методов"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "sources", comment: "Таблица для хранения источников информации", force: :cascade do |t|
+    t.string "name", null: false, comment: "Название источника"
+    t.text "description", comment: "Описание источника"
+    t.string "url", null: false, comment: "URL источника"
+    t.string "source_type", default: "сайт", null: false, comment: "Тип источника (сайт, группа в телеграмм)"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "telegram_in_comment_reactions", comment: "Таблица для хранения реакций на комментарии", force: :cascade do |t|
+    t.string "emoji", null: false, comment: "Смайл реакции"
+    t.integer "count", default: 1, null: false, comment: "Количество реакций данного вида"
+    t.bigint "telegram_in_comment_id", null: false, comment: "Ссылка на комментарий"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["telegram_in_comment_id"], name: "index_telegram_in_comment_reactions_on_telegram_in_comment_id"
+  end
+
+  create_table "telegram_in_comments", comment: "Таблица для хранения комментариев к сообщениям", force: :cascade do |t|
+    t.text "content", null: false, comment: "Текст комментария"
+    t.bigint "telegram_in_message_id", null: false, comment: "Ссылка на сообщение"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["telegram_in_message_id"], name: "index_telegram_in_comments_on_telegram_in_message_id"
+  end
+
+  create_table "telegram_in_groups", comment: "Таблица для хранения групп", force: :cascade do |t|
+    t.string "name", null: false, comment: "Название группы"
+    t.string "group_id", null: false, comment: "Уникальный идентификатор группы"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["group_id"], name: "index_telegram_in_groups_on_group_id", unique: true
+  end
+
+  create_table "telegram_in_media", comment: "Таблица для хранения медиафайлов", force: :cascade do |t|
+    t.text "file_data", null: false, comment: "Данные файла в формате Base64"
+    t.string "file_type", null: false, comment: "Тип файла (например, image/jpeg, video/mp4)"
+    t.bigint "telegram_in_message_id", null: false, comment: "Ссылка на сообщение"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["telegram_in_message_id"], name: "index_telegram_in_media_on_telegram_in_message_id"
+  end
+
+  create_table "telegram_in_message_reactions", comment: "Таблица для хранения реакций на сообщения", force: :cascade do |t|
+    t.string "emoji", null: false, comment: "Смайл реакции"
+    t.integer "count", default: 1, null: false, comment: "Количество реакций данного вида"
+    t.bigint "telegram_in_message_id", null: false, comment: "Ссылка на сообщение"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["telegram_in_message_id"], name: "index_telegram_in_message_reactions_on_telegram_in_message_id"
+  end
+
+  create_table "telegram_in_messages", comment: "Таблица для хранения сообщений", force: :cascade do |t|
+    t.text "html_content", null: false, comment: "HTML-текст сообщения"
+    t.integer "message_number", null: false, comment: "Номер сообщения"
+    t.bigint "telegram_in_group_id", null: false, comment: "Ссылка на группу"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "views_count", default: 0, null: false, comment: "Количество просмотров"
+    t.datetime "views_updated_at", precision: nil, comment: "Время последнего обновления количества просмотров"
+    t.boolean "original", default: true, null: false, comment: "Оригинальное сообщение или сообщение для переделки"
+    t.index ["telegram_in_group_id", "message_number"], name: "index_messages_on_group_id_and_number", unique: true
+    t.index ["telegram_in_group_id"], name: "index_telegram_in_messages_on_telegram_in_group_id"
+  end
+
+  add_foreign_key "contents", "settings"
+  add_foreign_key "telegram_in_comment_reactions", "telegram_in_comments"
+  add_foreign_key "telegram_in_comments", "telegram_in_messages"
+  add_foreign_key "telegram_in_media", "telegram_in_messages"
+  add_foreign_key "telegram_in_message_reactions", "telegram_in_messages"
+  add_foreign_key "telegram_in_messages", "telegram_in_groups"
+end
